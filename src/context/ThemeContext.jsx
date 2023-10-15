eaf47254c8812146896ad994a949fb048036c2d0
mohammed-bouaziz <63472622+mohammed-bouaziz@users.noreply.github.com>
2023-10-15 20:31:20 +0200

    menu-responsive

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,28 @@
+"use client";
+import { createContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext();
+
+const getFromLocalStorage = () => {
+
+    if (typeof window !== undefined){
+        const value = localStorage.getItem("theme")
+        return value || "light";
+    }
+    return "light"
+}
+
+export const ThemeContextProvider = ({children}) => {
+    const [theme, setTheme] = useState(() => {
+         return getFromLocalStorage();
+    });
+    const toggle = () => {
+        setTheme(theme === "light" ? "dark" : "light");
+    }
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+          localStorage.setItem("theme", theme);
+        }
+      }, [theme]);
+    return <ThemeContext.Provider value={{theme, toggle}}>{children}</ThemeContext.Provider>
+};
